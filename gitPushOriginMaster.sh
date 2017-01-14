b8b22116718b26cf9cdf8af1e33aac9ec30ac29a
louixs <ryuei.sasaki@gmail.com>
2017-01-14 14:53:22 +0000

    updated the deployment bash code

--- gitPushOriginMaster.sh
+++ gitPushOriginMaster.sh
@@ -2,6 +2,14 @@
 
 # For adding the whole stack, add a commit message and push from origin to master
 
+folder=$1 #folder in which log and .db files to be removed to make sure
+
+rm "$folder"/assets/*.db
+echo ".db files removed"
+rm -r "$folder"/assets/log
+echo "log files removed"
+
+
 git add .
-git commit -m "$1"
+git commit -m "$2"
 git push origin master
